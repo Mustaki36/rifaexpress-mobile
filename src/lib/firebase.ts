dd3169107b38d33ba5500f576c91e404710d2caa
Los Tacos Place <lostacosplace@gmail.com>
2025-08-05 21:19:40 +0000

    mejor usa este: // Import the functions you need from the SDKs you need

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAF3alOC7a1D7Q47pjkn4B_CwHZebt8kes",
-  authDomain: "rifas-xpress.firebaseapp.com",
-  projectId: "rifas-xpress",
-  storageBucket: "rifas-xpress.appspot.com",
-  messagingSenderId: "459527685686",
-  appId: "1:459527685686:web:2b798a250ece9ba32d3a97",
-  measurementId: "G-S1676RYJ3W"
+  apiKey: "AIzaSyC68jUWqV_62IvWxsQvzpzDNlos7ikczrM",
+  authDomain: "rifasexpress-b51e9.firebaseapp.com",
+  projectId: "rifasexpress-b51e9",
+  storageBucket: "rifasexpress-b51e9.appspot.com",
+  messagingSenderId: "4583104375",
+  appId: "1:4583104375:web:9ff56f07b2e589f222cb72",
+  measurementId: "G-DWGRY2Z306"
 };
 
 
@@ -24,3 +24,4 @@ const app = initializeApp(firebaseConfig);
 // Exportamos los servicios que usaremos en la aplicación.
 export const auth = getAuth(app); // Servicio de Autenticación
 export const db = getFirestore(app); // Servicio de Base de Datos (Firestore)
+
