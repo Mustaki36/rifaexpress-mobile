105a390876ea51e3933b9c01c84351affa5ade74
Los Tacos Place <lostacosplace@gmail.com>
2025-08-05 19:35:20 +0000

    mas lenta la transiscion de tarjeta de rifa al area de seleccion de nume

--- src/components/template.tsx
+++ src/components/template.tsx
@@ -9,7 +9,7 @@ const Template = ({ children }: { children: React.ReactNode }) => {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 20 }}
-      transition={{ ease: 'easeInOut', duration: 0.5 }}
+      transition={{ ease: 'easeInOut', duration: 1 }}
     >
       {children}
     </motion.div>
