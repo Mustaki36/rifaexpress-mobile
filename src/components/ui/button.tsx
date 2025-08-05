53085aefe1a4437097e58a5034a9055556ca7c31
Los Tacos Place <lostacosplace@gmail.com>
2025-08-05 19:48:30 +0000

    reduce el retardo a 125 milisegundos

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -58,7 +58,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         if (soundTimeoutRef.current) {
             clearTimeout(soundTimeoutRef.current);
         }
-        soundTimeoutRef.current = setTimeout(playHoverSound, 250);
+        soundTimeoutRef.current = setTimeout(playHoverSound, 125);
     };
 
     const handleMouseLeave = () => {
