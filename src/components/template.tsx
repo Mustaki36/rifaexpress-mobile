b157c05758d5efc14f4e4fa3fa044bf942ffb382
Los Tacos Place <lostacosplace@gmail.com>
2025-08-05 19:55:35 +0000

    mas lento super lento

--- src/components/template.tsx
+++ src/components/template.tsx
@@ -9,7 +9,7 @@ const Template = ({ children }: { children: React.ReactNode }) => {
       initial={{ opacity: 0, filter: 'blur(8px)' }}
       animate={{ opacity: 1, filter: 'blur(0px)' }}
       exit={{ opacity: 0, filter: 'blur(8px)' }}
-      transition={{ ease: 'easeInOut', duration: 1 }}
+      transition={{ ease: 'easeInOut', duration: 3 }}
     >
       {children}
     </motion.div>
